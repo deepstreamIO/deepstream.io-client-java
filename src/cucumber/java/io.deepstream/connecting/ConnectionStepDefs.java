279503b66ac0be5050550ce8d3973f1bb90d8552
Yasser Fadl <yasser.fadl@gmail.com>
2016-05-23 18:44:59 +0200

    Moving gradle cucumber tests

---
+++ src/cucumber/java/io.deepstream/connecting/ConnectionStepDefs.java
@@ -0,0 +1,87 @@
+package io.deepstream.connecting;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.deepstream.DeepstreamClient;
+import io.deepstream.util.MockTcpServer;
+import io.deepstream.constants.EndpointType;
+import io.deepstream.util.Util;
+import org.json.JSONObject;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConnectionStepDefs {
+
+    private final char MPS =  '\u001f';
+    private final char MS = '\u001e';
+
+    private MockTcpServer server;
+
+    {
+        try {
+            server = new MockTcpServer(9696);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DeepstreamClient client;
+    Properties options = new Properties();
+
+    @Before
+    public void beforeScenario() throws IOException, InterruptedException {
+        server.open();
+        Thread.sleep(10);
+    }
+
+    @After
+    public void afterScenario() throws IOException, InterruptedException {
+        server.close();
+    }
+
+    @Given("^the test server is ready$")
+    public void The_test_server_is_ready() throws Throwable {
+        Assert.assertEquals( true, server.isOpen );
+    }
+
+    @Given("^the client is initialised$")
+    public void the_client_is_initialised() throws Throwable {
+        options.setProperty( "endpoint", EndpointType.TCP.name() );
+        client = new DeepstreamClient("http://localhost:9696", options);
+    }
+
+    @Then("^the server has (\\d+) active connections$")
+    public void The_server_has_connections(int connections) throws Throwable {
+        Assert.assertEquals( connections, server.getNumberOfConnections() );
+    }
+
+    @Then("^the clients connection state is \"(.*?)\"$")
+    public void the_clients_connection_state_is(String arg1) throws Throwable {
+        Assert.assertEquals( arg1, client.getConnectionState().name() );
+    }
+
+    @Then("^the server sends the message (.*?)$")
+    public void The_server_sends_the_message(String message) throws Throwable {
+        message = message.replace( '|', MPS );
+        message = message.replace( '+', MS );
+        Thread.sleep(1000);
+        server.send( message );
+    }
+
+    @When("^the client logs in with username \"(.*?)\" and password \"(.*?)\"")
+    public void The_client_logs_in_with_username_and_password( String username, String password ) throws Exception {
+        String creds = String.format("{\"username\":\"%s\", \"password\":\"%s\" }", username, password);
+        JSONObject authData = new JSONObject( creds );
+        client.login( authData );
+    }
+
+    @Then("^the last message the server recieved is (.*?)$")
+    public void The_last_message_the_server_received_is( String message ) throws Exception {
+        Assert.assertEquals( message, Util.matchMessage( server.getLastMessage() ) );
+    }
+}
---
+++ src/cucumber/java/io.deepstream/util/MockTcpServer.java
@@ -0,0 +1,104 @@
+package io.deepstream.util;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+
+public class MockTcpServer {
+
+    ServerSocket serverSocket;
+    ArrayList<Socket> connections;
+    ArrayList<Thread> threads;
+    String lastMessage;
+    Socket lastSocket;
+    DataInputStream in;
+    DataOutputStream out;
+    public boolean isOpen;
+
+    public MockTcpServer( int port ) throws IOException {
+        serverSocket = new ServerSocket();
+        serverSocket.setReuseAddress( true );
+        serverSocket.bind(new InetSocketAddress(port));
+        connections = new ArrayList<>();
+        threads = new ArrayList<>();
+    }
+
+    public void open() {
+        this.isOpen = true;
+        final MockTcpServer self = this;
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Socket sock = serverSocket.accept();
+                    self.lastSocket = sock;
+                    self.handleConnection(sock);
+                } catch ( SocketTimeoutException e) {
+                    System.out.println( "SocketTimeoutException " + e );
+                } catch ( SocketException e ) {
+                    System.out.println( "SocketException " + e );
+                } catch (IOException e) {
+                    System.out.println( "IOException " + e );
+                }
+            }
+        };
+        thread.start();
+    }
+
+    private void handleConnection( final Socket sock ) {
+        this.connections.add( sock );
+        final MockTcpServer self = this;
+
+        Thread connectionThread = new Thread() {
+            @Override
+            public void run() {
+                while( self.isOpen ) {
+                    try {
+                        self.in = new DataInputStream(sock.getInputStream());
+                        self.out = new DataOutputStream(sock.getOutputStream());
+                    } catch (IOException e) {
+                        System.out.println( "IOException " + e );
+                    }
+
+                    try {
+                        String message = in.readUTF();
+                        self.lastMessage = message;
+                    } catch ( SocketException e) {
+                        System.out.println( "SocketException " + e );
+                    } catch (IOException e) {
+                        System.out.println( "IOException " + e );
+                    }
+                }
+            }
+        };
+        this.threads.add( connectionThread );
+        connectionThread.start();
+    }
+
+    public void send( String message ) throws IOException {
+        this.out.writeUTF( message );
+    }
+
+    public void close() throws InterruptedException, IOException {
+        this.isOpen = false;
+        for ( Socket sock : this.connections ) {
+            sock.close();
+        }
+        for ( Thread connectedThread : this.threads ) {
+            connectedThread.join(1);
+        }
+        serverSocket.close();
+        serverSocket = null;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public int getNumberOfConnections() {
+        return this.connections.size();
+    }
+}
