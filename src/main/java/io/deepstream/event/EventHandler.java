81320a93115bede8867cfcd02e812abed231fff4
Alex <alexbruceharley@gmail.com>
2016-07-08 21:18:06 +1200

    tidying up event handler

--- src/main/java/io/deepstream/event/EventHandler.java
+++ src/main/java/io/deepstream/event/EventHandler.java
@@ -2,14 +2,17 @@ package io.deepstream.event;
 
 import io.deepstream.DeepstreamClient;
 import io.deepstream.constants.Actions;
+import io.deepstream.constants.Event;
 import io.deepstream.constants.Topic;
 import io.deepstream.message.Connection;
 import io.deepstream.message.Message;
 import io.deepstream.message.MessageBuilder;
 import io.deepstream.message.MessageParser;
-import io.deepstream.utils.ResubscribeCallback;
-import io.deepstream.utils.ResubscribeNotifier;
-import io.deepstream.utils.Emitter;
+import io.deepstream.utils.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EventHandler implements ResubscribeCallback {
@@ -18,25 +21,36 @@ public class EventHandler implements ResubscribeCallback {
     private Map options;
     private Connection connection;
     private DeepstreamClient client;
+    private AckTimeoutRegistry ackTimeoutRegistry;
     private ResubscribeNotifier resubscribeNotifier;
+    private Map<String, Listener> listeners;
+    private List<String> subscriptions;
 
     public EventHandler(Map options, Connection connection, DeepstreamClient client ) {
         this.emitter = new Emitter();
         this.connection = connection;
         this.client = client;
+        this.listeners = new HashMap<>();
+        this.subscriptions = new ArrayList<>();
+        int subscriptionTimeout = (int) options.get( "subscriptionTimeout" );
+        this.ackTimeoutRegistry = new AckTimeoutRegistry( client, Topic.EVENT, subscriptionTimeout );
         this.resubscribeNotifier = new ResubscribeNotifier( this.client, this );
     }
 
     public void subscribe( String eventName, Emitter.Listener eventListener ) {
         if( this.emitter.hasListeners( eventName ) == false ) {
+            this.ackTimeoutRegistry.add( eventName, Actions.SUBSCRIBE );
             this.connection.send( MessageBuilder.getMsg( Topic.EVENT, Actions.SUBSCRIBE, eventName ) );
         }
+        this.subscriptions.add( eventName );
         this.emitter.on( eventName, eventListener );
     }
 
     public void unsubscribe( String eventName, Emitter.Listener eventListener ) {
+        this.subscriptions.remove( eventName );
         this.emitter.off(eventName, eventListener);
         if ( this.emitter.hasListeners(eventName) == false ) {
+            this.ackTimeoutRegistry.add( eventName, Actions.UNSUBSCRIBE );
             this.connection.send(MessageBuilder.getMsg(Topic.EVENT, Actions.UNSUBSCRIBE, eventName));
         }
     }
@@ -49,6 +63,27 @@ public class EventHandler implements ResubscribeCallback {
     public void emit( String eventName, Object data ) {
         this.connection.send( MessageBuilder.getMsg( Topic.EVENT, Actions.EVENT, eventName, MessageBuilder.typed( data ) ) );
         this.emitter.emit( eventName, data );
+    }
+
+    public void listen( String pattern, Emitter.Listener callback ) {
+        if( this.listeners.get( pattern ) != null ) {
+            this.client.onError( Topic.EVENT, Event.LISTENER_EXISTS, pattern );
+        } else {
+            this.listeners.put( pattern,
+                    new Listener(Topic.EVENT, pattern, callback, this.options, this.client, this.connection )
+            );
+        }
+    }
+
+    public void unlisten( String pattern ) {
+        Listener listener = this.listeners.get( pattern );
+        if( listener != null ) {
+            this.ackTimeoutRegistry.add( pattern, Actions.UNLISTEN );
+            listener.destroy();
+            this.listeners.remove( pattern );
+        } else {
+            this.client.onError( Topic.EVENT, Event.NOT_LISTENING, pattern );
+        }
     }
 
     public void handle( Message message ) {
@@ -67,18 +102,24 @@ public class EventHandler implements ResubscribeCallback {
                 this.emitter.emit( eventName );
             }
         }
+        else if( this.listeners.get( eventName ) != null ) {
+            this.listeners.get( eventName ).onMessage( message );
+        }
         else if( message.action == Actions.ACK ) {
-            System.out.println( "Recieved ack for " + eventName );
+            this.ackTimeoutRegistry.clear( message );
         }
         else if( message.action == Actions.ERROR ) {
-            System.out.println( "Recieved error for " + eventName );
-        }else {
-            System.out.println( "Unsoliciated message " + eventName );
+            this.client.onError( Topic.EVENT, Event.getEvent( message.data[ 0 ] ), message.data[ 1 ]);
+        }
+        else {
+            this.client.onError( Topic.EVENT, Event.UNSOLICITED_MESSAGE, eventName );
         }
     }
 
     @Override
     public void resubscribe() {
-
+        for ( String eventName : this.subscriptions ) {
+            this.connection.sendMsg( Topic.EVENT, Actions.SUBSCRIBE, new String[] { eventName } );
+        }
     }
 }
