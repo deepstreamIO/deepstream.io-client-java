5a422108c0403904cc1f9713fd29b58fb53a10e0
Yasser Fadl <yasser.fadl@gmail.com>
2016-08-03 19:45:15 +0200

    Adding binfrog upload

--- scripts/publishJar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl \
-    -T "build/libs/deepstream.io.*.jar" \
-    -H "X-Bintray-Publish:1" \
-    -u yasserf:$1 \
-    "https://api.bintray.com/content/deepstreamio/maven/deepstream.io-client-java/${1}/deepstream.io-client-java_${1}.jar"
--- scripts/setupJ2OBJC.sh
+++ scripts/setupJ2OBJC.sh
@@ -8,7 +8,7 @@ fi
 J2OBJC_ROOT=$TRAVIS_BUILD_DIR/j2objcDist
 
 # Create local.properties for gradlew
-#sed -i '' '/j2objc.home/d' $DIRECTORY/local.properties
+echo '# J2OBJC Start' >> $DIRECTORY/../local.properties;
 echo j2objc.home=$J2OBJC_ROOT/j2objc-$J2OBJCVersion >> $DIRECTORY/../local.properties
 if [[ "$TRAVIS_OS_NAME" == "osx" && "$OSTYPE" == "darwin"* ]]; then
 	echo 'Wooho! Mac! j2objc compilation!';
@@ -16,13 +16,14 @@ else
 	echo 'Only transpiling will occur';
 	echo j2objc.translateOnlyMode=true >> $DIRECTORY/../local.properties;
 fi
+echo '# J2OBJC End' >> $DIRECTORY/../local.properties;
 
 # Download J2OBJC
 mkdir -p $J2OBJC_ROOT;
 pushd $J2OBJC_ROOT;
 if [ ! -d "$J2OBJC_ROOT/j2objc-$J2OBJCVersion" ]; then
 	curl -L https://github.com/google/j2objc/releases/download/$J2OBJCVersion/j2objc-$J2OBJCVersion.zip > j2objc-$J2OBJCVersion.zip
-	unzip j2objc-$J2OBJCVersion.zip; popd
+	unzip -q j2objc-$J2OBJCVersion.zip; popd
 else
 	echo 'j2objc already downloaded';
 fi
