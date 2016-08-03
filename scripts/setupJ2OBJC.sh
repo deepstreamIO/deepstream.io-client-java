3be25e351b74af8f5e041e8685d5b5958464f690
Yasser Fadl <yasser.fadl@gmail.com>
2016-08-03 12:29:50 +0200

    Moving scripts into a folder, adding publish script

--- scripts/setupJ2OBJC.sh
+++ scripts/setupJ2OBJC.sh
@@ -3,18 +3,18 @@ J2OBJCVersion=1.1
 # Paths and Stuff
 DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 if [ -z ${TRAVIS_BUILD_DIR} ]; then
-	export TRAVIS_BUILD_DIR=$DIRECTORY;
+	export TRAVIS_BUILD_DIR=$DIRECTORY/..;
 fi
 J2OBJC_ROOT=$TRAVIS_BUILD_DIR/j2objcDist
 
 # Create local.properties for gradlew
 #sed -i '' '/j2objc.home/d' $DIRECTORY/local.properties
-echo j2objc.home=$J2OBJC_ROOT/j2objc-$J2OBJCVersion >> $DIRECTORY/local.properties
+echo j2objc.home=$J2OBJC_ROOT/j2objc-$J2OBJCVersion >> $DIRECTORY/../local.properties
 if [[ "$TRAVIS_OS_NAME" == "osx" && "$OSTYPE" == "darwin"* ]]; then
 	echo 'Wooho! Mac! j2objc compilation!';
 else
 	echo 'Only transpiling will occur';
-	echo j2objc.translateOnlyMode=true >> $DIRECTORY/local.properties;
+	echo j2objc.translateOnlyMode=true >> $DIRECTORY/../local.properties;
 fi
 
 # Download J2OBJC
