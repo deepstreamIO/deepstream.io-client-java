db9a2a66b879877edae497503d8d8e9760ccfd7c
Akram Hussein <akramhussein@gmail.com>
2016-09-29 23:00:53 +0100

    Added annotation to AnonymousRecordUninitialized.java

--- src/main/java/io/deepstream/AnonymousRecordUninitialized.java
+++ src/main/java/io/deepstream/AnonymousRecordUninitialized.java
@@ -1,5 +1,7 @@
 package io.deepstream;
 
+import com.google.j2objc.annotations.ObjectiveCName;
+
 /**
  * An exception that is thrown if you try to do a {@link AnonymousRecord#discard()} or {@link AnonymousRecord#delete()}
  * before a record has been set via {@link AnonymousRecord#setName(String)}
@@ -11,6 +13,7 @@ public class AnonymousRecordUninitialized extends Exception {
      * This Exception is thrown by {@see AnonymousRecord} and should not be constructed by consumers of this library
      * @param methodName The method name that was called
      */
+    @ObjectiveCName("init:")
     AnonymousRecordUninitialized(String methodName) {
         super( "Can`t invoke " + methodName + ". AnonymousRecord not initialised. Call setName first" );
     }
