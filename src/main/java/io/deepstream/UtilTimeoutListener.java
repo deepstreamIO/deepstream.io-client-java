d74676d1b1c104f7e10e68f6f71645618512d1e9
Akram Hussein <akramhussein@gmail.com>
2016-09-29 23:40:36 +0100

    Added annotation to UtilTimeoutListener.java

--- src/main/java/io/deepstream/UtilTimeoutListener.java
+++ src/main/java/io/deepstream/UtilTimeoutListener.java
@@ -1,5 +1,8 @@
 package io.deepstream;
 
+import com.google.j2objc.annotations.ObjectiveCName;
+
 interface UtilTimeoutListener {
+	@ObjectiveCName("onTimeout:action:event:name:")
     void onTimeout(Topic topic, Actions action, Event event, String name );
 }
