d499a097ee366c78cf1ff56f71b7715551e28de5
Akram Hussein <akramhussein@gmail.com>
2017-03-18 16:57:07 +0000

    ObjectiveC Name

--- src/main/java/io/deepstream/EndpointFactory.java
+++ src/main/java/io/deepstream/EndpointFactory.java
@@ -1,8 +1,11 @@
 package io.deepstream;
 
+import com.google.j2objc.annotations.ObjectiveCName;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
 interface EndpointFactory {
+	@ObjectiveCName("createEndpoint:connection:")
     public Endpoint createEndpoint(URI uri, Connection connection) throws URISyntaxException;
 }
