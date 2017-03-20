8004cf01a1878039cebd71f756ea108ce3b0b052
John Diamond <johndiamond@gmx.co.uk>
2017-03-20 18:38:08 +0100

    chore(ios): remove print statements

--- swift/Deepstream+Extensions.swift
+++ swift/Deepstream+Extensions.swift
@@ -217,25 +217,18 @@ public final class IOSEndpointWebsocket : NSObject, Endpoint {
     private var webSocket : WebSocket? {
         didSet {
             self.webSocket?.onConnect = {
-                print("Websocket: websocketDidConnect")
                 self.connection.onOpen()
             }
 
             self.webSocket?.onDisconnect = { (error: NSError?) in
-                print("Websocket: websocketDidDisconnect")
-                if (error != nil) {
-                    print("Websocket: \(error?.localizedDescription)")
-                }
                 self.connection.onClose()
             }
 
             self.webSocket?.onText = { (text: String) in
-                print("Websocket: websocketDidReceiveMessage, \(text)")
                 self.connection.onMessage(text)
             }
 
             self.webSocket?.onData = { (data: Data) in
-                print("Websocket [Unhandled): websocketDidReceiveData \(data)")
             }
 
         }
@@ -247,29 +240,23 @@ public final class IOSEndpointWebsocket : NSObject, Endpoint {
     }
 
     public func send(_ message: String!) {
-        print("IOSEndpointWebsocket: \(message!)")
         self.webSocket?.write(string: message)
     }
 
     public func close() {
-        print("IOSEndpointWebsocket: Close")
         self.webSocket?.disconnect()
         self.webSocket = nil
-        self.connection.onClose()
     }
 
     public func forceClose() {
-        print("Websocket: Force close")
         self.webSocket?.disconnect(forceTimeout: nil, closeCode: 1)
     }
 
     public func open() {
         guard let url = URL(string: self.uri.toASCIIString()) else {
-            print("IOSEndpointWebsocket Error: Invalid url")
             return
         }
 
-        print("IOSEndpointWebsocket: Open on \(url)")
         self.webSocket = WebSocket(url: url)
         self.webSocket?.connect()
     }
